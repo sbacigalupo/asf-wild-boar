c67eb1028a06596f1b98612aec7f26c27842bd84
Sonny Bacigalupo <sbacigalupo@rvc.ac.uk>
2021-08-23 15:05:54 +0100

    Fixes
    Changes:
    - the death probability gave a lifespan of one year, not ten (line 97)
    - the births weren't stochastic (line 179)
    - now the death rate is density dependent, it's better to use a Poisson distribution for determining how many occur as the rate may go above one (lines 181-183)
    - the density-dependent birth and death rates need to be calculated for the boar population as a whole, not for each infection class (i.e. N=S+E+I, not S, E and I separately) (lines 175 & 177)
    - density-dependent birth and death rates always >=0

--- SEID_model.R
+++ SEID_model.R
@@ -1,5 +1,5 @@
 # Version 2 - working version
-# Frequency dependent transmission
+# Density dependent transmission
 
 library(tidyverse)
 
@@ -52,7 +52,7 @@ init_pop_e <- rep(0,n.patch)
 init_pop_e[1] <- 0  # Number of pre-infectious
 
 init_pop_i <- rep(0,n.patch)
-init_pop_i[1] <- 1  # Number of infectious
+init_pop_i[1] <- 0  # Number of infectious
 
 init_pop_d <- rep(0,n.patch)
 init_pop_d[1] <- 0  # Number of dead
@@ -75,7 +75,7 @@ c_trace_tab[1,"forest","D"]
 beta <-0.75/1500        # density dependent would be contact rate*transmission rate
 
 beta_wbp <- 1/30/1500 # Contact rate between wild boar and farm (1 visit per 30 days per 1500 wild boar)
-                     # assumes 100% transmission at the moment
+# assumes 100% transmission at the moment
 
 zeta <- 1/7 # Pre-infectious to infectious (latent period) 7 days
 
@@ -83,6 +83,7 @@ infection_death <- 1/10 # 5 day lifespan
 
 # Read in movement data (per week)
 move_data <- read_csv("move_matrix.csv",col_names = T) %>% as.matrix.data.frame()
+#move_data <- read.csv("move_matrix.csv",header = T)
 
 # Convert data to daily rate
 move_data_daily_0 <- move_data
@@ -91,18 +92,16 @@ move_data_daily_0 <- move_data
 # i.e. columns sum up to 1
 move_data_daily_0 <- move_data_daily_0/colSums(move_data_daily_0)
 
+##################### SG EDITED FROM HERE
 # Set up birth, death parameters per day
-birth_per_capita <- 0.45*5/365 # Assumes 90% of females (50% of population) have 5 piglets per year
+births_per_capita <- 0.45*5/365 # Assumes 90% of females (50% of population) have 5 piglets per year
 cull_probability <- 0.5/365
-death_probability <- (1/365 + cull_probability) # Assumes lifespan 10 years
-carrying_capacity <- c(1e4,300,1e5) # How many boar sustainable in different areas
+deaths_per_capita <- 1/3650   # Assumes lifespan 10 years
+carrying_capacity <- c(1200,240,60) # How many boar sustainable in different areas
 daily_sd_movement <- 0.4 # Daily measure of variation in movement
-a <- 0.6 # constant for density dependent growth
-growth_rate = birth_per_capita - death_probability
-
-
-
-
+a <- 0.6                 # constant for density dependent growth (between 0 and 1)
+growth_rate <- births_per_capita - deaths_per_capita
+##################### SG EDITED TO HERE
 
 # Run model ---------------------------------------------------------------
 
@@ -125,7 +124,7 @@ for(tt in 2:max_time){ # iterate over days
     # Add randomness to probability of movement
     move_data_daily <- matrix(rlnorm(9,mean=log(move_data_daily_0),sd=daily_sd_movement),nrow=n.patch,byrow=F) # generate random daily values for movement
     move_data_daily <- t(t(move_data_daily)/colSums(move_data_daily)) # Make sure everything sums up to 1. eg sum(move_data_daily[1])
-
+    
     # Calculate susceptible transitions
     pop_f_to_f <- rbinom(1,  pop_time_tt[1,pick_p],move_data_daily[1,1]) # How many stay in forest
     pop_f_to_b <- rbinom(1,  pop_time_tt[1,pick_p] -pop_f_to_f,move_data_daily[2,1]/(move_data_daily[2,1]+move_data_daily[3,1])) # Of those that leave forest, how many go to border
@@ -138,7 +137,7 @@ for(tt in 2:max_time){ # iterate over days
     pop_o_to_f <- rbinom(1,  pop_time_tt[3,pick_p],move_data_daily[1,3]) # How many go to forest
     pop_o_to_b <- rbinom(1,  pop_time_tt[3,pick_p]-pop_o_to_f,move_data_daily[2,3]/(move_data_daily[2,3]+move_data_daily[3,3])) # Of rest, how many go to border
     pop_o_to_o <- as.integer(pop_time_tt[3,pick_p] - pop_o_to_f - pop_o_to_b) # The rest must go to outside
-  
+    
     # Tally up new populations after movement
     new_pop_f <- pop_f_to_f+pop_b_to_f+pop_o_to_f
     new_pop_b <- pop_f_to_b+pop_b_to_b+pop_o_to_b
@@ -158,30 +157,22 @@ for(tt in 2:max_time){ # iterate over days
   new_total[,"S"] <- pmax(new_total[,"S"] - S_to_E,0)  # (+ check S >=0)
   new_total[,"E"] <- pmax(new_total[,"E"] + S_to_E - E_to_I,0)
   new_total[,"I"] <- pmax(new_total[,"I"] + E_to_I - I_to_death,0)
-  new_total[,"D"] <- new_total[,"D"] + I_to_death
+  new_total[,"D"] <- pmax(new_total[,"D"] + I_to_death,0)
   
+################### SG EDITED FROM HERE
   # Births and deaths into different compartments
-#  scaled_births_with_carrying_capacity <- birth_per_capita*pmax(0,(1-new_total[,"S"]/carrying_capacity) ) # Calculate birth rate, scaling to reduce as nearer carrying capacity
-#  new_births_S<- rpois(n_population,lambda = new_total[,"S"]*scaled_births_with_carrying_capacity) # Add Poisson births
-  
-  # Add deaths
-#  new_deaths_S <- rbinom(n_population,size=new_total[,"S"],prob=death_probability) # Add deaths
-#  new_deaths_E <- rbinom(n_population,size=new_total[,"E"],prob=death_probability) # Add deaths
-#  new_deaths_I <- rbinom(n_population,size=new_total[,"I"],prob=death_probability) # Add deaths
-  
-  
-  # Density dependent coefficients
-  
-  density_dependent_birth_coeff <- (a*growth_rate*new_total/carrying_capacity)# *new_total  # if doesn't run, remove hash here and line below. remove *total from new_
 
-  density_dependent_death_coeff <- ((1-a)*growth_rate*new_total/carrying_capacity)#*new_total 
-  
-  new_births_S <- round((birth_per_capita - density_dependent_birth_coeff[,"S"])*new_total[,"S"], digits = 0)
-  
-  new_deaths_S <- round((rbinom(n_population,size=new_total[,"S"],prob=death_probability + density_dependent_death_coeff[,"S"])), digits=0) # Add deaths
-  new_deaths_E <- round((rbinom(n_population,size=new_total[,"E"],prob=death_probability + density_dependent_death_coeff[,"E"])), digits=0) # Add deaths
-  new_deaths_I <- round((rbinom(n_population,size=new_total[,"I"],prob=death_probability + density_dependent_death_coeff[,"I"])), digits=0) # Add deaths
+  # Density dependent birth and death rates
+  birth_rate <- pmax(births_per_capita - a*growth_rate*colSums(new_total[,1:3])/carrying_capacity,0) #SB amended to ensure >0
+  death_rate <- pmax(deaths_per_capita + (1-a)*growth_rate*colSums(new_total[,1:3])/carrying_capacity,0) #SB amended to ensure >0
 
+  new_births_S <- rpois(n_population, lambda = birth_rate*colSums(new_total[,1:3])) #SB amended
+  
+  new_deaths_S <- rpois(n_population, lambda = death_rate*new_total[,"S"]) #SB amended
+  new_deaths_E <- rpois(n_population, lambda = death_rate*new_total[,"E"]) #SB amended
+  new_deaths_I <- rpois(n_population, lambda = death_rate*new_total[,"I"]) #SB amended
+  
+################### SG EDITED TO HERE
   # might need to add new growth rate for next iteration based on actual difference in rates
   
   # Store new populations
@@ -191,21 +182,20 @@ for(tt in 2:max_time){ # iterate over days
   c_trace_tab[tt,,"D"] <- new_total[,"D"]
   
   # Calculate force of infection
- 
- # for(tt in 2:max_time){ # iterate over days
-    outputs_tt <- o_trace_tab[tt-1,,] # Values at the start of the day
-    new_outputs <- outputs_tt         # Matrix to store new values
   
-    foi <- beta*new_total[,"I"]       # Force of infection between wild boar in each area
-    wb_p <- beta_wbp*new_total[,"I"]  # Force of infection from wild boar on farm
-    
-    new_outputs[,"i_force"] <- foi
-    new_outputs[,"boar-pig_inf"] <- wb_p
-    
-    o_trace_tab[tt,,"i_force"] <- new_outputs[,"i_force"]
-    o_trace_tab[tt,,"boar-pig_inf"] <- new_outputs[,"boar-pig_inf"]
-    
-  }
+  outputs_tt <- o_trace_tab[tt-1,,] # Values at the start of the day
+  new_outputs <- outputs_tt         # Matrix to store new values
+  
+  foi <- beta*new_total[,"I"]       # Force of infection between wild boar in each area
+  wb_p <- beta_wbp*new_total[,"I"]  # Force of infection from wild boar on farm
+  
+  new_outputs[,"i_force"] <- foi
+  new_outputs[,"boar-pig_inf"] <- wb_p
+  
+  o_trace_tab[tt,,"i_force"] <- new_outputs[,"i_force"]
+  o_trace_tab[tt,,"boar-pig_inf"] <- new_outputs[,"boar-pig_inf"]
+
+}
 
 
 # Plot outplots -----------------------------------------------------------
@@ -220,34 +210,34 @@ c_trace_tab[1:5,,"I"]
 
 c_trace_tab[1:5,,"D"]
 
-#c_trace_tab[1:30,,"FOI"]
+o_trace_tab[1:30,,"i_force"]
 
 
 # Plot outputs by n.patch
 {
-par(mfrow=c(3,2))
-
-col_pick_states <- list("blue","orange","red","black")
-col_pick_patch <- list("darkgreen","brown","orange")
-
-{plot(c_trace_tab[,,],col="white", 
-     main = "Wild boar numbers in the forest", xlab = "Days", ylab = "Number of individuals", 
-     xlim=c(0,max_time),ylim=c(1,max(c_trace_tab[,"forest",])))
-
-
-for(dd in 1:n.state){
-  lines(c_trace_tab[,"forest",dd],col=col_pick_states[[dd]])
-  }
-}
-
-{plot(c_trace_tab[,,],col="white", 
-     main = "Wild boar numbers at the border", xlab = "Days", ylab = "Number of individuals", 
-     xlim=c(0,max_time),ylim=c(1,max(c_trace_tab[,"border",])))
+  par(mfrow=c(3,2))
   
-  for(dd in 1:n.state){
-    lines(c_trace_tab[,"border",dd],col=col_pick_states[[dd]])
+  col_pick_states <- list("blue","orange","red","black")
+  col_pick_patch <- list("darkgreen","brown","orange")
+  
+  {plot(c_trace_tab[,,],col="white", 
+        main = "Wild boar numbers in the forest", xlab = "Days", ylab = "Number of individuals", 
+        xlim=c(0,max_time),ylim=c(1,max(c_trace_tab[,"forest",])))
+    
+    
+    for(dd in 1:n.state){
+      lines(c_trace_tab[,"forest",dd],col=col_pick_states[[dd]])
+    }
   }
-}
+  
+  {plot(c_trace_tab[,,],col="white", 
+        main = "Wild boar numbers at the border", xlab = "Days", ylab = "Number of individuals", 
+        xlim=c(0,max_time),ylim=c(1,max(c_trace_tab[,"border",])))
+    
+    for(dd in 1:n.state){
+      lines(c_trace_tab[,"border",dd],col=col_pick_states[[dd]])
+    }
+  }
   
   {plot(c_trace_tab[,,],col="white", 
         main = "Wild boar numbers outside the forest", xlab = "Days", ylab = "Number of individuals", 
@@ -265,31 +255,31 @@ for(dd in 1:n.state){
              col = c("blue","orange","red","black","white","white","white"), lty = 1, border = "black", cex=1)
     }
   }
-
+  
   {plot(o_trace_tab[,,],col="white", 
-       main = "Force of infection (beta*I) over time", xlab = "Days", ylab = "Force of infection", 
-       xlim=c(0,max_time),
-       ylim=c(0,max(o_trace_tab[,,"i_force"])))
-       legend("topright", legend = c("inside","border","outside"), 
-       col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
-  
-  
-  for(dd in 1:n.patch){
-    lines(o_trace_tab[,dd,"i_force"],col=col_pick_patch[[dd]])
+        main = "Force of infection (beta*I) over time", xlab = "Days", ylab = "Force of infection", 
+        xlim=c(0,max_time),
+        ylim=c(0,max(o_trace_tab[,,"i_force"])))
+    legend("topright", legend = c("inside","border","outside"), 
+           col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+    
+    
+    for(dd in 1:n.patch){
+      lines(o_trace_tab[,dd,"i_force"],col=col_pick_patch[[dd]])
+    }
   }
+  {plot(o_trace_tab[,,],col="white", 
+        main = "Force of infection (beta*I) on one farm over time\nassuming contact with farm leads to transmission\n(same as number of new infections)", xlab = "Days", ylab = "Force of infection", 
+        xlim=c(0,max_time),
+        ylim=c(0,max(o_trace_tab[,,"boar-pig_inf"])))
+    legend("topright", legend = c("inside","border","outside"), 
+           col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+    
+    
+    for(dd in 1:n.patch){
+      lines(o_trace_tab[,dd,"boar-pig_inf"],col=col_pick_patch[[dd]])
+    }
   }
-{plot(o_trace_tab[,,],col="white", 
-      main = "Force of infection (beta*I) on one farm over time\nassuming contact with farm leads to transmission\n(same as number of new infections)", xlab = "Days", ylab = "Force of infection", 
-      xlim=c(0,max_time),
-      ylim=c(0,max(o_trace_tab[,,"boar-pig_inf"])))
-  legend("topright", legend = c("inside","border","outside"), 
-         col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
-  
-  
-  for(dd in 1:n.patch){
-    lines(o_trace_tab[,dd,"boar-pig_inf"],col=col_pick_patch[[dd]])
-  }
-}
 }
 
 
