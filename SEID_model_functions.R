e2c21961df7981b9a7ce88dc27e77a773f6b87f8
Sonny Bacigalupo <sbacigalupo@rvc.ac.uk>
2021-10-06 08:59:56 +0100

    Update SEID_model_functions.R
    Runs outbreak over scenarios. Plots individual run from particular scenario. Plots median run from particular scenario.

--- SEID_model_functions.R
+++ SEID_model_functions.R
@@ -1,4 +1,10 @@
-outbreak <- function(beta){
+#################################################################
+##
+## Run ASF outbreak model
+##
+#################################################################
+
+outbreak <- function(beta, beta_wbp,cull_effort){
   # Set up model parameters ------------------------------------------------------------
   
   #Set up numbers
@@ -6,9 +12,6 @@ outbreak <- function(beta){
   
   # Setting up an array 
   max_time =365
-  beta_wbp = 1/30/1500
-  zeta = 1/7
-  infection_death = 1/10
   
   patchNames=c("forest","border","outside")
   stateNames=c("S","E","I","D","prob_wb_wb","prob_wb_p")
@@ -71,9 +74,8 @@ outbreak <- function(beta){
   
   # Set up birth, death parameters per day
   births_per_capita <- 0.45*5/365 # Assumes 90% of females (50% of population) have 5 piglets per year
-  cull_probability <- 0.5/365
   deaths_per_capita <- 1/3650   # Assumes lifespan 10 years
-  carrying_capacity <- c(1200,240,60) # How many boar sustainable in different areas
+  carrying_capacity <- c(5000,240,100000) # How many boar sustainable in different areas
   daily_sd_movement <- 0.4 # Daily measure of variation in movement
   a <- 0.5                 # constant for density dependent growth (between 0 and 1)
   growth_rate <- births_per_capita - deaths_per_capita
@@ -119,6 +121,11 @@ outbreak <- function(beta){
     }
     
     # Add disease transitions
+    
+    zeta = 1/7              # E->I pre-infectious/latent period
+    infection_death = 1/10  # I->D infectious period 
+
+    
     # removed /rowSums(new_total) from line 120
     
     S_to_E <- rpois(3,lambda=beta*new_total[,"S"]*new_total[,"I"] ) # generate random infections 
@@ -137,11 +144,13 @@ outbreak <- function(beta){
     birth_rate <- pmax(births_per_capita - a*growth_rate*colSums(new_total[,1:3])/carrying_capacity,0) #SB amended to ensure >0
     death_rate <- deaths_per_capita + (1-a)*growth_rate*colSums(new_total[,1:3])/carrying_capacity
     
+    cull_rate <- (pmax(birth_rate - death_rate,0))*cull_effort # base cull rate (pop~1500)*change in cull effort
+    
     new_births_S <- rpois(n_population, lambda = birth_rate*colSums(new_total[,1:3])) #SB amended
     
-    new_deaths_S <- rpois(n_population, lambda = death_rate*new_total[,"S"]) #SB amended
-    new_deaths_E <- rpois(n_population, lambda = death_rate*new_total[,"E"]) #SB amended
-    new_deaths_I <- rpois(n_population, lambda = death_rate*new_total[,"I"]) #SB amended
+    new_deaths_S <- rpois(n_population, lambda = (death_rate+cull_rate)*new_total[,"S"]) #SB amended
+    new_deaths_E <- rpois(n_population, lambda = (death_rate+cull_rate)*new_total[,"E"]) #SB amended
+    new_deaths_I <- rpois(n_population, lambda = (death_rate+cull_rate)*new_total[,"I"]) #SB amended
     
     ################### SG EDITED TO HERE
     # might need to add new growth rate for next iteration based on actual difference in rates
@@ -163,20 +172,25 @@ outbreak <- function(beta){
     c_trace_tab[tt,,"prob_wb_wb"] <- prob_wb
     c_trace_tab[tt,,"prob_wb_p"] <- prob_pig
     
-    
   }
-  
-  
-#  Model<-list(array(NA,dim=c(n_run_pick,length(max_time)),dimnames=list(NULL,NULL)))
-  
-#  return(Model)
   
   return("c_trace_tab" = c_trace_tab)
 }
 
 
+#################################################################
+##
+## Plotting Functions
+##
+#################################################################
 
-  # Plot outputs by n.patch
+
+
+#----------------------------------------------------------------
+# Plot outbreak when looped over n_run only
+#----------------------------------------------------------------
+
+
   plot_outbreak <- function(n_run){
     
     pop_trace <- store[n_run,,,]
@@ -255,11 +269,203 @@ outbreak <- function(beta){
   }
   
   
-  ###########################################################
-  ##
-  ## Return total probability of transmission to pigs "prob_wb_p"
-  ##
-  ###########################################################
+  
+  
+#----------------------------------------------------------------
+# Plot individual outbreak when looping over multiple scenarios and n_run
+#----------------------------------------------------------------  
+  
+  
+  
+  plot_individual_run <- function(cull_effort, beta_interspecies, beta, n_run){
+    
+    pop_trace <- store_scenarios[cull_effort, beta_interspecies, beta, n_run,,,]
+    
+    max_time <- length(pop_trace[,"forest","S"])
+    n.state <- length(pop_trace[1,"forest",])
+    n.patch <- length(pop_trace[1,,"S"])
+    
+    par(mfrow=c(3,2))
+    
+    
+    col_pick_states <- list("blue","orange","red","black")
+    col_pick_patch <- list("darkgreen","brown","orange")
+    
+    {plot(pop_trace[,,],col="white", 
+          main = "Wild boar numbers in the forest", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(pop_trace[,"forest",])))
+      
+      
+      for(dd in 1:4){
+        lines(pop_trace[,"forest",dd],col=col_pick_states[[dd]])
+      }
+    }
+    
+    {plot(pop_trace[,,],col="white", 
+          main = "Wild boar numbers at the border", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(pop_trace[,"border",])))
+      
+      for(dd in 1:4){
+        lines(pop_trace[,"border",dd],col=col_pick_states[[dd]])
+      }
+    }
+    
+    {plot(pop_trace[,,],col="white", 
+          main = "Wild boar numbers outside the forest", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(pop_trace[,"outside",])))
+      
+      for(dd in 1:4){
+        lines(pop_trace[,"outside",dd],col=col_pick_states[[dd]])
+      }
+      
+      {plot(pop_trace[,,],col="white",axes = FALSE, xlab = "", ylab = "",
+            main = "Legend", 
+            xlim=c(0,max_time),ylim=c(1,max(pop_trace[,"outside",])))
+        
+        legend("center",legend = c("susceptible","pre-infectious","infectious","dead"), 
+               col = c("blue","orange","red","black"), lty = 1, border = "black", cex=1)
+      }
+    }
+    
+    {plot(pop_trace[,,],col="white", 
+          main = "Probability of infection between \nwild boar over time", xlab = "Days", ylab = "Probability of infection", 
+          xlim=c(0,max_time),
+          ylim=c(0,max(pop_trace[,,"prob_wb_wb"])))
+      legend("topright", legend = c("inside","border","outside"), 
+             col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+      
+      
+      for(ee in 1:n.patch){
+        lines(pop_trace[,ee,"prob_wb_wb"],col=col_pick_patch[[ee]])
+      }
+    }
+    {plot(pop_trace[,,],col="white", 
+          main = "Probability of infection between \nwild boar and pigs over time", xlab = "Days", ylab = "Probability of infection", 
+          xlim=c(0,max_time),
+          ylim=c(0,max(pop_trace[,,"prob_wb_p"])))
+      legend("topright", legend = c("inside","border","outside"), 
+             col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+      
+      
+      for(ee in 1:n.patch){
+        lines(pop_trace[,ee,"prob_wb_p"],col=col_pick_patch[[ee]])
+      }
+      mtext(paste0("Cull Effort=",cull_effort,",   Interspecies Transmission=", beta_interspecies,",    Beta =",beta,",   Run ",n_run), line = -1, cex = 0.85, outer = TRUE)
+    }
+  }
+  
+  
+  
+#----------------------------------------------------------------
+# Plot median outbreak for all scenarios
+#----------------------------------------------------------------
+  
+  
+plot_median_outbreak <- function(cull_effort, beta_interspecies, beta){
+    
+    scenario <- store_scenarios[cull_effort, beta_interspecies, beta,,,,]
+    
+    max_time <- length(store_scenarios[1,1,1,1,,"forest","S"])
+    n.state <- length(store_scenarios[1,1,1,1,1,"forest",])
+    n.patch <- length(store_scenarios[1,1,1,1,1,,"S"])
+    patchNames=c("forest","border","outside")
+    stateNames=c("S","E","I","D","prob_wb_wb","prob_wb_p")
+    beta_names <- c("0.7","1","1.3","1.6","1.9","2.2")
+    beta_interspecies_names <- c("1/30","7/30","14/30") # median visitation rate, maximum visitation rate, upper 95% CI visitation rate
+    cull_names <- c("none","cull_rate","1.5*cull_rate","2*cull_rate")
+    
+    store_medians <- array(dim=c(max_time,n.patch,n.state),dimnames=list(NULL,patchNames,stateNames))
+    
+    for (xx in 1:max_time){
+      for(yy in 1:n.patch){
+        for(zz in 1:n.state){
+          store_medians[xx,yy,zz] <- median(scenario[,xx,yy,zz])
+        } # End loop
+      }
+    }
+    
+    par(mfrow=c(3,2))
+    
+    
+    col_pick_states <- list("blue","orange","red","black")
+    col_pick_patch <- list("darkgreen","brown","orange")
+    
+    
+    {plot(store_scenarios[cull_effort,beta_interspecies,beta,1,,,],col="white", 
+          main = "Wild boar numbers in the forest", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(store_medians[,"forest",]))
+    )
+      
+      for(dd in 1:4){
+        lines(store_medians[,"forest",dd],col=col_pick_states[[dd]])
+      }
+    }
+    
+    {plot(store_medians[,,],col="white", 
+          main = "Wild boar numbers at the border", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(store_medians[,"border",])))
+      
+      for(dd in 1:4){
+        lines(store_medians[,"border",dd],col=col_pick_states[[dd]])
+      }
+    }
+    
+    {plot(store_medians[,,],col="white", 
+          main = "Wild boar numbers outside the forest", xlab = "Days", ylab = "Number of individuals", 
+          xlim=c(0,max_time),ylim=c(1,max(store_medians[,"outside",])))
+      
+      for(dd in 1:4){
+        lines(store_medians[,"outside",dd],col=col_pick_states[[dd]])
+      }
+      
+      {plot(store_medians[,,],col="white",axes = FALSE, xlab = "", ylab = "",
+            main = "Legend", 
+            xlim=c(0,max_time),ylim=c(1,max(store_medians[,"outside",])))
+        
+        legend("center",legend = c("susceptible","pre-infectious","infectious","dead"), 
+               col = c("blue","orange","red","black"), lty = 1, border = "black", cex=1)
+      }
+    }
+    
+    {plot(store_medians[,,],col="white", 
+          main = "Probability of infection between \nwild boar over time", xlab = "Days", ylab = "Probability of infection", 
+          xlim=c(0,max_time),
+          ylim=c(0,max(store_medians[,,"prob_wb_wb"])))
+      legend("topright", legend = c("inside","border","outside"), 
+             col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+      
+      
+      for(ee in 1:n.patch){
+        lines(store_medians[,ee,"prob_wb_wb"],col=col_pick_patch[[ee]])
+      }
+    }
+    {plot(store_medians[,,],col="white", 
+          main = "Probability of infection between \nwild boar and pigs over time", xlab = "Days", ylab = "Probability of infection", 
+          xlim=c(0,max_time),
+          ylim=c(0,max(store_medians[,,"prob_wb_p"])))
+      legend("topright", legend = c("inside","border","outside"), 
+             col = c("darkgreen","brown","orange"), lty = 1, border = "black", cex=1)
+      
+      
+      for(ee in 1:n.patch){
+        lines(store_medians[,ee,"prob_wb_p"],col=col_pick_patch[[ee]])
+      }
+      mtext(paste0("Cull Effort=",cull_effort,",    Interspecies Transmission=", beta_interspecies,",    Beta =",beta), line = -1, cex = 0.85, outer = TRUE)
+    }
+  }
+  
+  
+  
+###########################################################
+##
+## Return outputs when looping over n_run only
+##
+###########################################################
+  
+  
+#----------------------------------------------------------------
+# Return total probability of transmission to pigs "prob_wb_p" 
+#----------------------------------------------------------------
   
 infect_pig <- function(n_run){
   store_table <- NULL
@@ -298,39 +504,68 @@ infect_pig <- function(n_run){
   write_csv(store_table,paste0("median_infectpig.csv"))
 }
 
-###########################################################
-##
-## Return number of wild boar dead (outbreak size)
-##
-###########################################################
+  
+  
+#----------------------------------------------------------------
+# Return number of wild boar dead (outbreak size)
+#----------------------------------------------------------------
 
 size <- function(n_run){
-  store_table <- NULL
-  store_size <- NULL
+  store_table <- NULL # Matrix to store summary data
+  store_size <- NULL  # Matrix to store individual run data
   for(kk in 1:n_run){
-    pop_trace <- store[kk,,,]
+    pop_trace <- store[kk,,,] # Select each individual run
+    
+    # Calculate outbreak size
     outbreak_size <- sum(max(pop_trace[,"forest","D"]),
                          max(pop_trace[,"border","D"]),
                          max(pop_trace[,"outside","D"]))
     
+    # Store values
     store_size<-rbind(store_size,c(kk,outbreak_size))
-  }
+  } # end run loop
+  
   store_size <- as_tibble(store_size)
+  
   names(store_size) <- c("run","outbreak_size")
   
     write_csv(store_size,paste0("store_size.csv"))
+    
+    # Calculate outputs
   
-  store_table <- rbind(store_table,c(median(store_size$outbreak_size),quantile(store_size$outbreak_size,c(0.025,0.975))))
+    no_outbreak <- sum(store_size$outbreak_size < 100) # Count number of outbreaks with <100 infected deaths (no outbreak)
+    
+    # Store summary data
+  store_table <- rbind(store_table,c(median(store_size$outbreak_size), # median outbreak size
+                                     min(store_size$outbreak_size),    # minimum outbreak size
+                                     max(store_size$outbreak_size),    # maximum outbreak size
+                                     quantile(store_size$outbreak_size,c(0.025,0.975)),  # 95% confidence interval
+                                     no_outbreak,                      # number of non-outbreaks       
+                                     (no_outbreak/(max(n_run)))*100    # % of non-outbreaks
+                                     )
+                       )
   
   # Convert
   store_table <- as_tibble(store_table)
-  names(store_table) <- c("outbreak_size_med","outbreak_size_95_1","outbreak_size_95_2")
-  store_table$outbreak_size_med <- as.numeric(store_table$outbreak_size_med)
-  store_table$outbreak_size_95_1 <- as.numeric(store_table$outbreak_size_95_1)
-  store_table$outbreak_size_95_2 <- as.numeric(store_table$outbreak_size_95_2)
+  names(store_table) <- c("median_outbreak_size",
+                          "min_outbreak_size", 
+                          "max_outbreak_size",
+                          "lower95CI_outbreak_size",
+                          "upper95CI_outbreak_size",
+                          "number_no_outbreak",
+                          "prop_no_outbreak")
+  store_table$median_outbreak_size <- as.numeric(store_table$median_outbreak_size)
+  store_table$min_outbreak_size <- as.numeric(store_table$min_outbreak_size)
+  store_table$max_outbreak_size <- as.numeric(store_table$max_outbreak_size)
+  store_table$lower95CI_outbreak_size <- as.numeric(store_table$lower95CI_outbreak_size)
+  store_table$upper95CI_outbreak_size <- as.numeric(store_table$upper95CI_outbreak_size)
+  store_table$number_no_outbreak <- as.numeric(store_table$number_no_outbreak)
+  store_table$prop_no_outbreak <- as.numeric(store_table$prop_no_outbreak)
 
   
    write_csv(store_table,paste0("median_size.csv"))
+   
+   return(outputs = as.data.frame(store_table)) 
 }
   
     
